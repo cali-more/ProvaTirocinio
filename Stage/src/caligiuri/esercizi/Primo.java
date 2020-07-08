09f93a9073d753c4cb95e01cebb1d2f92a13cdb5
Morena <Morena@DESKTOP-FROO13V>
2020-07-08 21:51:08 +0200

    nuovo esercizio

--- Stage/src/caligiuri/esercizi/Primo.java
+++ Stage/src/caligiuri/esercizi/Primo.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Primo {
 
 	public static Scanner in = new Scanner(System.in);
-	
+
 	public static boolean primo ( int x ) {
 		for (int i = 2; i <= Math.sqrt(x) ; i++) {
 			if ( x % i == 0 ) {
@@ -17,13 +17,20 @@ public class Primo {
 		}
 		return true;
 	}
-	
+
 	public static void main(String[] args) {
 
 		System.out.println("Inserisci un numero intero positivo: ");
 		int n = in.nextInt();
-		System.out.print(primo (n));
-		
+		System.out.println(primo (n));
+		System.out.println("La lista dei numeri primi da 1 a " + n + " è:");
+
+		for (int i = 1; i <= n; i++) {
+			if (primo(i)) {
+				System.out.println( " " + i + " ");
+			}
+		}
+
 	}
 
 }
